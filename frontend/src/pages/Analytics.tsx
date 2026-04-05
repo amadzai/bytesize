1422c7092cdb90ee191f3fb997824271c0b2bc2d
amadzai <36450453-amadzai@users.noreply.gitlab.com>
2026-04-05 13:49:26 +0800

    fix: update typo

--- frontend/src/pages/Analytics.tsx
+++ frontend/src/pages/Analytics.tsx
@@ -95,7 +95,7 @@ export function Analytics() {
           </div>
           <div>
             <h1 className="text-foreground text-xl font-bold md:text-3xl">
-              Plaform Analytics
+              Platform Analytics
             </h1>
             <p className="text-muted-foreground text-xs md:text-base">
               View all shortened URLs and their visit analytics
