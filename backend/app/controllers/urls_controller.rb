bb0484bdb61dfb11184587993676327de8093b65
amadzai <36450453-amadzai@users.noreply.gitlab.com>
2026-04-05 14:03:14 +0800

    refactor: remove redundant helper

--- backend/app/controllers/urls_controller.rb
+++ backend/app/controllers/urls_controller.rb
@@ -3,7 +3,7 @@ class UrlsController < ApplicationController
   rate_limit to: 60, within: 1.minute, only: :index, with: RATE_LIMIT_EXCEEDED
 
   def shorten
-    target_url = url_params[:target_url]
+    target_url = params.permit(:target_url)[:target_url]
 
     url = Url.new(target_url: target_url)
     url.validate
@@ -66,11 +66,5 @@ class UrlsController < ApplicationController
     redirect_to url.target_url, status: :found, allow_other_host: true
   rescue ActiveRecord::RecordNotFound
     render json: { error: "Short URL not found" }, status: :not_found
-  end
-
-  private
-
-  def url_params
-    params.permit(:target_url)
   end
 end
